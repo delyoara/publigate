988a1f3c57ef13fc2cd991f42eebfbeed34348dd
petronela <delyoara@yahoo.fr>
2025-08-26 22:34:11 +0200

    WIP : routes for authentication

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from journals import views
-from users.views import RegisterView
+from users.views import register, login, me, request_password_reset, reset_password, change_password
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -14,7 +14,15 @@ urlpatterns = [
     path("api/journals/", views.journals, name="journals"),
     path("api/journals/names/", views.journal_names, name="journal_names"),
     path("api/journals/<int:id>/", views.journal_detail, name="journal_detail"),
-    path('api/register/', RegisterView.as_view(), name='register'),
+    path('api/register/', register, name='register'),
+
+    path('api/login/', login, name='login'),
+    path('api/me/', me, name='me'),
+    path('api/request-password-reset/', request_password_reset, name='request-password-reset'),
+    path('api/reset-password/', reset_password, name='reset-password'),
+    path('api/change-password/', change_password, name='change-password'),
+
+
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
